b835c062b17a93eae864d95d06adf831b2ac7ebb
Joe <jpalumb0@chicagobooth.edu>
2017-07-17 16:26:00 +0000

    updates thru payment form

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -19,6 +19,20 @@ class CalculationsController < ApplicationController
         render("/calculations/flexible_square_root_template.html.erb")
     end
     
+    def flex_payment
+        @apr = params[:interest_rate].to_f
+        @years = params[:term_years].to_i
+        @principal = params[:principal].to_f
+        
+        @apr = @apr/100
+        @apr = @apr.round(2)
+        
+        @monthly_payment = @principal *( ((@apr / 100 / 12) * ( 1 + (@apr / 100 / 12))**(@years * 12))  / ( ( 1 + (@apr / 100 / 12) )**(@years * 12) - 1) )
+     
+        
+    render("/calculations/flexible_payment_template.html.erb")
+    end
+    
     def flex_random
         # The incoming parameters for this action look like {"user_number" => "bob"}
         # Rails stores that hash in a variable called params
@@ -31,4 +45,49 @@ class CalculationsController < ApplicationController
         
         render("/calculations/flexible_random_template.html.erb")
     end
+    
+    def square_form
+        
+        
+        render("/calculations/square_form_template.html.erb")
+    end
+    
+    def process_square
+       
+        @user_number = params["the_user_number"].to_f
+        @squared_number = @user_number**2
+       
+       render("/calculations/square_results_template.html.erb") 
+    end
+    
+    def square_root_form
+        
+        
+        render("/calculations/square_root_form_template.html.erb")
+    end
+    
+    def process_square_root
+       
+        @user_number = params["the_user_number"].to_f
+        @square_root_number = @user_number**0.5
+       
+       render("/calculations/square_root_results_template.html.erb") 
+    end
+    
+    def payment_form
+       
+       render("/calculations/payment_form_template.html.erb") 
+    end
+    
+    def process_payment
+        
+        @apr = params["user_APR"].to_f
+        @years = params["user_term"].to_i
+        @principal = params["user_principal"].to_f
+        
+        
+        @monthly_payment = @principal *( ((@apr / 100 / 12) * ( 1 + (@apr / 100 / 12))**(@years * 12))  / ( ( 1 + (@apr / 100 / 12) )**(@years * 12) - 1) )
+     
+        render("/calculations/payment_results_template.html.erb")
+    end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   
   get("/flexible/square/:user_number",{:controller => "calculations",:action => "flex_square"})
   get("/flexible/square_root/:user_number",{:controller => "calculations",:action => "flex_square_root"})
+  get("/flexible/payment/:interest_rate/:term_years/:principal",{:controller => "calculations",:action => "flex_payment"})
   get("/flexible/random/:range_1/:range_2",{:controller => "calculations",:action => "flex_random"})
   
+  get("/square/new",{:controller => "calculations",:action => "square_form"})
+  get("/square/results",{:controller => "calculations",:action => "process_square"})
+  
+  get("/square_root/new",{:controller => "calculations",:action => "square_root_form"})
+  get("/square_root/results",{:controller => "calculations",:action => "process_square_root"})
+  
+  get("/payment/new", {:controller => "calculations", :action => "payment_form"})
+  get("/payment/results", {:controller => "calculations", :action => "process_payment"})
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
